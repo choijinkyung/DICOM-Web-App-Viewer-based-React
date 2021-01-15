bb406c0e008e4e240af382e7b429ef5345c6c3a0
cjk <twin7014@naver.com>
2021-01-15 10:30:38 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
-function App() {
+
+// Routes
+import Viewer from './Viewer/Viewer.js';
+
+function ExampleEntry({ title, url, text }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h5>
+          <Link to={url}>{title}</Link>
+        </h5>
+        <p>{text}</p>
+        <hr />
+      </div>
   );
 }
 
-export default App;
+function Index() {
+  const style = {
+    minHeight: '512px',
+  };
+
+  const examples = [
+    {
+      title: 'Open Viewer',
+      url: '/grid',
+      text: 'multiple viewers',
+    }
+  ];
+
+  const exampleComponents = examples.map(e => {
+    return <ExampleEntry key={e.title} {...e} />;
+  });
+
+  return (
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12 col-lg-12" style={style}>
+            <h1>Cloud App PACS</h1>
+            {exampleComponents}
+          </div>
+        </div>
+      </div>
+  );
+}
+
+function Login(props) {
+  return (
+      <div className="container">
+        <h5>
+          <Link to="/">Back to Login</Link>
+        </h5>
+        {props.children}
+      </div>
+  );
+}
+
+function AppRouter() {
+  const grid = () => Login({ children: <Viewer /> });
+  return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Index} />
+          <Route exact path="/grid/" render={grid} />
+
+          <Route exact component={Index} />
+        </Switch>
+      </Router>
+  );
+}
+
+export default class App extends Component {
+  render() {
+    return <AppRouter />;
+  }
+}
